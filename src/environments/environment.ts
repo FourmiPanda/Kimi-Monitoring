c35fafd19b65147dd9e79a32b5aba0a79586f4f0
FourmiPanda <ismail.el-khantache@etu.univ-nantes.fr>
2018-11-17 14:04:19 +0100

    Clean project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCGDmHp1RN511loQgxeSeeeBeFaWek-pIc",
-    authDomain: "loginang-7034e.firebaseapp.com",
-    databaseURL: "https://loginang-7034e.firebaseio.com",
-    projectId: "loginang-7034e",
-    storageBucket: "loginang-7034e.appspot.com",
-    messagingSenderId: "528303106160"
+    apiKey: 'AIzaSyCGDmHp1RN511loQgxeSeeeBeFaWek-pIc',
+    authDomain: 'loginang-7034e.firebaseapp.com',
+    databaseURL: 'https://loginang-7034e.firebaseio.com',
+    projectId: 'loginang-7034e',
+    storageBucket: 'loginang-7034e.appspot.com',
+    messagingSenderId: '528303106160'
   }
 };
